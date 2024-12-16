b34369f082852c28cf35e96f7a0deb2691af92e1
somin00 <smhs12@naver.com>
2024-12-16 19:58:28 +0900

    [소민] 해시 - 완주하지 못한 선수

---
+++ 해시/완주하지 못한 선수/소민/code.js
@@ -0,0 +1,10 @@
+function solution(participant, completion) {
+  const pMap = new Map();
+
+  participant.map((name) => pMap.set(name, (pMap.get(name) | 0) + 1));
+  completion.map((name) => pMap.set(name, (pMap.get(name) | 0) - 1));
+
+  for (let [key, value] of pMap) {
+    if (value > 0) return key;
+  }
+}
