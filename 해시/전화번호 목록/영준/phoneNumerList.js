5016e6d72b052693b750cc98180f6691e4341fd4
cloudedpanther <rickeykoh11@gmail.com>
2024-12-22 22:44:18 +0900

    [영준] 전화번호 목록

---
+++ 해시/전화번호 목록/영준/phoneNumerList.js
@@ -0,0 +1,24 @@
+const solution = (phone_book) => {
+    const prefixSet = new Set();
+    const numSet = new Set();
+
+    return phone_book.every((num) => {
+        if(numSet.has(num)) {
+            return false;
+        }
+        
+        for(let i = 0, j = num.length, prefix = ''; i < j; i++) {
+            prefix += num[i];
+            
+            if(prefixSet.has(prefix)) {
+                return false;
+            }
+            
+            numSet.add(prefix);
+        }
+        
+        prefixSet.add(num)
+        
+        return true;
+    })
+}
