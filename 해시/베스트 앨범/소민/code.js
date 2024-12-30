7ac4dfd424d6258a31421548858a51f919690240
frontend-somin <oh.somin@cashwalk.io>
2024-12-30 18:29:42 +0900

    [소민] 해시 - 베스트 앨범

---
+++ 해시/베스트 앨범/소민/code.js
@@ -0,0 +1,31 @@
+function solution(genres, plays) {
+  const answer = [];
+  const map = {};
+
+  for (let i = 0; i < genres.length; i++) {
+    const prevObj = map[genres[i]];
+    map[genres[i]] = {
+      sum: (prevObj?.sum || 0) + plays[i],
+      arr: [...(prevObj?.arr || []), [i, plays[i]]],
+    };
+  }
+
+  const sortedgenre = Object.keys(map).sort((a, b) => map[b].sum - map[a].sum);
+
+  for (let i = 0; i < sortedgenre.length; i++) {
+    map[sortedgenre[i]].arr.sort((a, b) => {
+      if (a[1] === b[1]) return a[0] - b[0];
+      else return b[1] - a[1];
+    });
+
+    if (map[sortedgenre[i]].arr.length === 1) {
+      answer.push(map[sortedgenre[i]].arr[0][0]);
+    } else {
+      for (let j = 0; j < 2; j++) {
+        answer.push(map[sortedgenre[i]].arr[j][0]);
+      }
+    }
+  }
+
+  return answer;
+}
