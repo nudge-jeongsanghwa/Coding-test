c7f35fd8b96eef43413d65164f4d455d857431ea
frontend-somin <oh.somin@cashwalk.io>
2024-12-30 18:30:08 +0900

    [소민] 스택과 큐 - 같은 숫자는 싫어

---
+++ 스택과 큐/같은 숫자는 싫어/소민/code.js
@@ -0,0 +1,9 @@
+function solution(arr) {
+  const answer = [];
+  for (let i = 0, j = arr.length; i < j; i++) {
+    if (arr[i] !== arr[i + 1]) {
+      answer.push(arr[i]);
+    }
+  }
+  return answer;
+}
