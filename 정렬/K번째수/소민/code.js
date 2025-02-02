7208250b011f0520241836d3eaa1472ccb3b527b
somin00 <smhs12@naver.com>
2025-02-02 21:36:34 +0900

    [소민]정렬 - k번째수

---
+++ 정렬/K번째수/소민/code.js
@@ -0,0 +1,10 @@
+function solution(array, commands) {
+  const answer = [];
+
+  for (let idx = 0; idx < commands.length; idx++) {
+    const [i, j, k] = commands[idx];
+    answer.push(array.slice(i - 1, j).sort((a, b) => a - b)[k - 1]);
+  }
+
+  return answer;
+}
